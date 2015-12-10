59d5cb1a7a32bc7c047a15b843934d07adc3a8c9
Michael Cavalea <callmecavs@gmail.com>
2015-12-10 12:37:44 -0500

    batch element clientHeight calls

--- src/packer.js
+++ src/packer.js
@@ -7,32 +7,31 @@ export default (options = {}) => {
   const container = document.querySelector(options.container)
   const elements = toArray(options.elements)
 
+  // cache element heights
+  const elHeights = elements.map(element => element.clientHeight)
+
   // initialize column heights
-  let heights = times(options.columns, 0)
+  let colHeights = times(options.columns, 0)
 
   // go through the items
-  elements.forEach(element => {
-    // get height
-    // TODO: batch calls to get all element heights, separate loop
-    let elementH = element.clientHeight
-
+  elements.forEach((element, index) => {
     // get shortest column index
-    let target = heights.indexOf(Math.min(...heights))
+    let target = colHeights.indexOf(Math.min(...colHeights))
 
     // calculate transform for current element
-    let top = heights[target]
+    let top = colHeights[target]
     let left = (target * options.width) + (target * options.gutter)
 
     element.style.position = 'absolute'
     element.style.transform = `translate3d(${ left }px, ${ top }px, 0)`
 
     // update current column height - add element height and gutter
-    heights[target] = heights[target] + elementH + options.gutter
+    colHeights[target] = colHeights[target] + elHeights[index] + options.gutter
   })
 
   // set container height based on tallest column
   container.style.position = 'relative'
-  container.style.height = `${ Math.max(...heights) - options.gutter }px`
+  container.style.height = `${ Math.max(...colHeights) - options.gutter }px`
 
   console.timeEnd('Masonry')
 }
