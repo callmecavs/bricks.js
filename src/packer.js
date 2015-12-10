0befcb554c7d301030f434d77db324b3f945ea7c
Michael Cavalea <callmecavs@gmail.com>
2015-12-10 11:38:22 -0500

    convert to use translate3d grid

--- src/packer.js
+++ src/packer.js
@@ -13,11 +13,11 @@ const times = (number, result) => {
 }
 
 export default (options = {}) => {
+  // cache elements and container
   const container = document.querySelector(options.container)
   const elements = toArray(options.elements)
 
-  // make the columns
-  const columns = times(options.columns, document.createDocumentFragment())
+  // initialize column heights
   let heights = times(options.columns, 0)
 
   // go through the items
@@ -25,13 +25,26 @@ export default (options = {}) => {
     // get height
     let elementH = element.clientHeight
 
-    // figure out shortest column
+    // get shortest column index
     let target = heights.indexOf(Math.min(...heights))
 
-    // add current element to shortest column
+    // add current element to it
+    let top = heights[target] === 0
+      ? heights[target]
+      : heights[target] + options.gutter
 
-    // update current column width
-    // add current element height, and gutter
+    let left = target === 0
+      ? target * options.width
+      : (target * options.width) + (target * options.gutter)
+
+    element.style.position = 'absolute'
+    element.style.transform = `translate3d(${ left }px, ${ top }px, 0)`
+
+    // update current column width - add element height and gutter
     heights[target] = heights[target] + elementH + options.gutter
   })
+
+  // set container height based on tallest column
+  container.style.position = 'relative'
+  container.style.height = `${ Math.max(...heights) }px`
 }
